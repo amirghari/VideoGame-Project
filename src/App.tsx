12ee6e17c44c78f036fcd69982641e9b7bc74597
Amir Ghari <amir.ghari@metropolia.fi>
2023-09-25 17:49:46 +0300

    Dark mode switch is done!

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg={'gray'}>
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg={'red'}>
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   )
 }
