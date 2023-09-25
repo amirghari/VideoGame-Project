34e83ec4b4819ab1aec6cceff937d43df44b39ef
Amir Ghari <amir.ghari@metropolia.fi>
2023-09-25 14:49:24 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import { Button } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  return <Button colorScheme="blue">Button</Button>
+}
+
+export default App
