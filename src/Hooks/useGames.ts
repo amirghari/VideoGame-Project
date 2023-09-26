74c4370420a35d5aefd98a3d16874a4cf43aeed7
Amir Ghari <amir.ghari@metropolia.fi>
2023-09-26 17:19:52 +0300

    DOne

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -14,14 +14,18 @@ const useGames = () => {
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState()
 
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>('/games')
-      .then((res) => {
-        setGames(res.data.results)
-        console.log(res.data)
-      })
-      .catch((err) => setError(err.message))
+useEffect(() => {
+  const controller = new AbortController()
+  apiClient
+    .get<FetchGameResponse>('/games', { signal: controller.signal})
+    .then((res) => setGames(res.data.results))
+    .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+    }
+        )
+
+    return () => controller.abort()
   }, [])
 
     return { games, error }
